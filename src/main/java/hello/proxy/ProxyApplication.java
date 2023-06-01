1f9cbb41f6e5e5907d2af49a86753c24f3200813
chahyoungseok <xcohdsuts@gmail.com>
2023-06-01 14:20:42 +0900

    feature : "예제 프로젝트 설정하기 V2 - 인터페이스 없는 구체 클래스 {스프링 Bean 으로 수동 등록}"

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,7 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
-
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Import;
  *
  * **/
 
-@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
---
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,35 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Controller 를 쓰지않은 이유는 해당 Controller, Service, Repository 를 수동 등록하는 예제이기에
+ * @ComponentScan 의 대상이 되는 것을 방지하기 위함입니다.
+ * **/
+
+@Slf4j
+@RequestMapping
+@ResponseBody
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
