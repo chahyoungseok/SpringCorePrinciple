635814061989fbaa711893b193f9c7fff96c2a79
chahyoungseok <xcohdsuts@gmail.com>
2023-06-04 23:50:57 +0900

    feature : "빈 후처리기의 쓰임과 테스트 진행"

---
+++ src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,70 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BasicTest {
+
+    @Test
+    void basicConfig(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanProcessorConfig.class);
+
+        // beanA 이름으로 B 객체가 빈으로 등록된다.
+        B b = applicationContext.getBean("beanA", B.class);
+        b.helloB();
+
+        // A는 빈으로 등록되지 안흔ㄴ다.
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BeanProcessorConfig {
+
+        @Bean(name = "beanA")
+        public A a(){
+            return new A();
+        }
+
+        @Bean
+        public AToBProcessor helloPostProcessor() {
+            return new AToBProcessor();
+        }
+    }
+
+    @Slf4j
+    static class A{
+        public void helloA(){
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B{
+        public void helloB(){
+            log.info("hello B");
+        }
+    }
+
+    @Slf4j
+    static class AToBProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={} bean={}", beanName, bean);
+
+            if (bean instanceof A) {
+                return new B();
+            }
+
+            return bean;
+        }
+    }
+}
