b010f507c44c669cadbf4364b7445b7c2ea1298d
chahyoungseok <xcohdsuts@gmail.com>
2023-06-10 22:05:40 +0900

    feature : "실패하면 재시도하는 기능을 AOP로 적용"

--- src/main/java/hello/aop/exam/aop/TraceAspect.java
+++ src/main/java/hello/aop/exam/aop/TraceAspect.java
@@ -2,11 +2,8 @@ package hello.aop.exam.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 @Slf4j
