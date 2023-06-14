c751dbc890f553f805c46cc5ead4895e675bd5dd
chahyoungseok <xcohdsuts@gmail.com>
2023-06-14 22:59:18 +0900

    초기 세팅

---
+++ src/test/java/com/example/mylittleannotation/MylittleannotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylittleannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylittleannotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
